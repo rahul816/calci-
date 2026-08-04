6f8773cfcdde4848b859b3de93d0c1df6f13d3d5
unknown <rahulnairsingh@gmail.com>
2026-08-04 12:44:58 +0530

    first commit

---
+++ cal.js
@@ -0,0 +1,20 @@
+function add (a,b) {
+return a+b;
+};
+function sub (a,b) {
+return a-b;
+};
+function mul (a,b) {
+return a*b;
+};
+function div(a,b) {
+return a/b;
+};
+const result1 = add(9, 16);
+console.log(result1); 
+const result2 = sub(88, 70);
+console.log(result2); 
+const result3 = mul(5, 3);
+console.log(result3); 
+const result4 = div(55, 5);
+console.log(result4); 
